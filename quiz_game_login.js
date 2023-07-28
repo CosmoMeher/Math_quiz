fcdad7093e9e22000ab9c4501d59ab2b26e885e6
CosmoMeher <124118520+CosmoMeher@users.noreply.github.com>
2023-07-27 21:27:07 -0700

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -16,5 +16,7 @@ function addUser(){
     player2_name=document.getElementById("player2_name_input").value;
 localStorage.setItem("player1_name", player1_name);
 localStorage.setItem("player2_name", player2_name);
-window.location="game_page.html";
+
 }
+
+window.location="game_page.html";
